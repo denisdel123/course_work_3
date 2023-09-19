0246df8e574332bbedb4c79e5fdfe36d993c2bf6
denisbelenko <denisdel123@>
2023-09-19 12:58:24 +0800

    закончил логику, разместил описания

--- untils/main.py
+++ untils/main.py
@@ -1,12 +1,21 @@
-from untils import get_operation, operation_executed
-
+from untils import get_operation, operation_executed, check_time, enter_info, print_info
 
 file_json = 'data/operations.json'
 
-
 if __name__ == '__main__':
+
     operations = get_operation(file_json)
-    operation_executed(operations)
+
+    for_check = operation_executed(operations)
+
+    from_check = check_time(for_check)
+
+    from_enter_info = enter_info(from_check, for_check)
+
+    print_info(from_enter_info)
+
+
+
 
 
 
