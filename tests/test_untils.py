5d2bfa130331f571e3346cb3213b9a25d288d1c1
denisbelenko <denisdel123@>
2023-09-20 13:59:31 +0800

    добавил тесты

--- tests/test_untils.py
+++ tests/test_untils.py
@@ -1,5 +1,4 @@
-import json
-from datetime import datetime
+
 
 import pytest
 
@@ -7,7 +6,7 @@ from untils import untils, main
 
 
 def test_get_operation():
-    assert untils.get_operation('../data/operations.json') != list
+    assert untils.get_operation('data/operations.json') != list
 
 
 def test_operation_executed():
@@ -29,20 +28,64 @@ def test_operation_executed():
 
 
 def test_check_time():
-    assert untils.check_time([{
-        "id": 441945886,
-        "state": "EXECUTED",
-        "date": "13, 07, 2019, 18, 51, 29".split(', '),
-        "operationAmount": {
-            "amount": "31957.58",
-            "currency": {
-                "name": "руб.",
-                "code": "RUB"
-            }
-        },
-        "description": "Перевод организации",
-        "from": "Maestro 1596837868705199",
-        "to": "Счет 64686473678894779589"
-    }]) != list
+    with pytest.raises(AssertionError):
+        assert untils.check_time([{
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "13, 07, 2019, 18, 51, 29".split(', '),
+            "operationAmount": {
+                "amount": "31957.58",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Maestro 1596837868705199",
+            "to": "Счет 64686473678894779589"
+        }]) == list
+
+
+def test_enter_info():
+    with pytest.raises(IndexError):
+        time = [['26', '08', '2019', '10', '50', '58'], ['26', '08', '2019', '10', '50', '58'],
+                ['26', '08', '2019', '10', '50', '58'], ['26', '08', '2019', '10', '50', '58']]
+        oper = [{
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "13, 07, 2019, 18, 51, 29".split(', '),
+            "operationAmount": {
+                "amount": "31957.58",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Maestro 1596837868705199",
+            "to": "Счет 64686473678894779589"
+        }]
+        assert untils.enter_info(time, oper) == list
+
+def test_print_info():
+    with pytest.raises(AssertionError):
+        result = [{
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "13, 07, 2019, 18, 51, 29".split(', '),
+            "operationAmount": {
+                "amount": "31957.58",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Maestro 1596837868705199",
+            "to": "Счет 64686473678894779589"
+        }]
+        assert untils.print_info(result) == list
+
+
 
 
--- untils/untils.py
+++ untils/untils.py
@@ -9,7 +9,6 @@ def get_operation(info):
     with open(info, 'r', encoding='utf-8') as file:
         operations = json.load(file)
 
-    print(type(operations))
     return operations
 
 
@@ -55,12 +54,10 @@ def check_time(oper):
 
         list_date.append(t1)
 
-    print(sorted(list_date))
     return sorted(list_date)
 
 
 def enter_info(time, oper):
-    print(oper)
     """ищу в json первые 5 дат которые добавил в массив и выписываю весь словарь в список"""
     operation_list = []
     cont = 0
